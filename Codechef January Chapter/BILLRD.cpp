2552c89d093ab943a45d0d425b7281694a686452
Kushagra Goel <goel.k874@gmail.com>
2021-01-04 16:50:06 +0530

    first commit

---
+++ Codechef January Chapter/BILLRD.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+#define T 100000
+#define K 1000000000
+#define N 1000000000
+
+int main()
+{
+    int t,n,k;
+    cin>>t;
+    cin>>n>>k;
+    cout<<n<<" "<<k;
+}
